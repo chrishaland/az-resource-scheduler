46163af3de66928860c14c2a89cb2706aa6fe681
Chris Håland <chris.haland@knowit.no>
2021-04-08 18:59:44 +0200

    View for starting environments

--- frontend/src/scheduler/Scheduler.js
+++ frontend/src/scheduler/Scheduler.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Locale } from '../translations/locale';
+import { locales } from './locales';
+import { useEnvironments } from '../environments/hooks';
+import { ScheduleItem } from './ScheduleItem';
+import './styles.css';
 
 export const Scheduler = () => {
+    const { environments } = useEnvironments();
+
     return (
         <div>
-            <h1>Hello, world!</h1>
+            <h1>
+                <Locale id={"header"} locales={locales}>Start Environments</Locale>
+            </h1>
+            {environments.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0)).map((environment, index) => (
+                <ScheduleItem key={index} environment={environment} />
+            ))}
         </div>
     );
 }
