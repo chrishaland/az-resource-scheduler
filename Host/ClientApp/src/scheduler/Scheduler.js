8fd6b463dc6d3e0f9945919669ccdaeedc15dca1
Chris Håland <chris.haland@outlook.com>
2021-12-15 22:43:36 +0100

    Implement environment uptime handler

--- Host/ClientApp/src/scheduler/Scheduler.js
+++ Host/ClientApp/src/scheduler/Scheduler.js
@@ -1,21 +1,32 @@
 import React from 'react';
+import { Table } from 'reactstrap';
 import { Locale } from '../translations/locale';
 import { locales } from './locales';
 import { useEnvironments } from '../environments/hooks';
+import { useEnvironmentUptimes } from './hooks';
 import { ScheduleItem } from './ScheduleItem';
 import './styles.css';
 
 export const Scheduler = () => {
     const { environments } = useEnvironments();
+    const { environmentUptimes } = useEnvironmentUptimes();
 
     return !!environments && environments.length > 0 ? (
         <div>
-            <h1>
+            <h4>
                 <Locale id={"header"} locales={locales}>Start Environments</Locale>
-            </h1>
-            {environments.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0)).map((environment, index) => (
-                <ScheduleItem key={index} environment={environment} />
-            ))}
+            </h4>
+            <Table hover striped>
+                <tbody>
+                    {environments.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0)).map((environment, index) => (
+                        <tr key={index}>
+                            <td scope="row">
+                                <ScheduleItem environment={environment} environmentUptime={environmentUptimes[environment.id]} />
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
         </div>
     ) : null;
 }
---
+++ Tests.IntegrationTests/Schedules/Get_environment_uptimes_tests.cs
@@ -0,0 +1,96 @@
+﻿using Service.Schedules;
+
+namespace Tests.IntegrationTests.Schedules;
+
+[TestFixture]
+public class Get_environment_uptimes_tests
+{
+    [Test]
+    public async Task Return_list_of_environment_uptimes()
+    {
+        await SUT.Database.DeleteAllAsync<Resource>(); 
+        await SUT.Database.DeleteAllAsync<Environment>();
+        await SUT.Database.DeleteAllAsync<ResourceStopJob>();
+
+        var scheduledStopTime = DateTime.Now.AddHours(2);
+        var entity = await SUT.Database.Environments.AddAsync(new Environment { Name = "dev", Description = "Development", ScheduledStartup = "* 1 * * *", ScheduledUptime = 1, Resources = new List<Resource> { } });
+
+        var provider = await SUT.Database.Providers.AddAsync(new Provider { Name = "dev" });
+        var resource = await SUT.Database.Resources.AddAsync(new VirtualMachine { Name = "vm", ResourceGroup = "rg", Description = "VM", Provider = provider.Entity, Environments = new List<Environment> { entity.Entity } });
+        
+        await SUT.Database.ResourceStopJobs.AddAsync(new ResourceStopJob { JobId = Guid.NewGuid().ToString(), Resource = resource.Entity, StopAt = scheduledStopTime });
+        await SUT.Database.SaveChangesAsync();
+
+        var request = new HttpRequestMessage(HttpMethod.Post, "/api/schedule/environment-uptimes/get");
+
+        var (response, content) = await SUT.SendHttpRequest<GetEnvironmentUptimesResponse>(request, new { });
+
+        if (content == null) throw new AssertionException("Http response did not return a object");
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.That(content.EnvironmentUptimes.Keys.Count, Is.EqualTo(1));
+
+            Assert.That(content.EnvironmentUptimes[entity.Entity.Id], Is.Not.Null);
+            Assert.That(content.EnvironmentUptimes[entity.Entity.Id].ResourcesCount, Is.EqualTo(1));
+            Assert.That(content.EnvironmentUptimes[entity.Entity.Id].ScheduledStopTime, Is.EqualTo(scheduledStopTime));
+        });
+    }
+
+    [Test]
+    public async Task Earliest_scheduled_stop_time_wins()
+    {
+        await SUT.Database.DeleteAllAsync<Resource>();
+        await SUT.Database.DeleteAllAsync<Environment>();
+        await SUT.Database.DeleteAllAsync<ResourceStopJob>();
+
+        var scheduledStopTime1 = DateTime.Now.AddHours(2);
+        var scheduledStopTime2 = DateTime.Now.AddHours(3);
+        var entity = await SUT.Database.Environments.AddAsync(new Environment { Name = "dev", Description = "Development", ScheduledStartup = "* 1 * * *", ScheduledUptime = 1, Resources = new List<Resource> { } });
+
+        var provider = await SUT.Database.Providers.AddAsync(new Provider { Name = "dev" });
+        var resource1 = await SUT.Database.Resources.AddAsync(new VirtualMachine { Name = "vm1", ResourceGroup = "rg", Description = "VM", Provider = provider.Entity, Environments = new List<Environment> { entity.Entity } });
+        var resource2 = await SUT.Database.Resources.AddAsync(new VirtualMachine { Name = "vm2", ResourceGroup = "rg", Description = "VM", Provider = provider.Entity, Environments = new List<Environment> { entity.Entity } });
+        
+        await SUT.Database.ResourceStopJobs.AddAsync(new ResourceStopJob { JobId = Guid.NewGuid().ToString(), Resource = resource1.Entity, StopAt = scheduledStopTime1 });
+        await SUT.Database.ResourceStopJobs.AddAsync(new ResourceStopJob { JobId = Guid.NewGuid().ToString(), Resource = resource2.Entity, StopAt = scheduledStopTime2 });
+
+        await SUT.Database.SaveChangesAsync();
+
+        var request = new HttpRequestMessage(HttpMethod.Post, "/api/schedule/environment-uptimes/get");
+
+        var (response, content) = await SUT.SendHttpRequest<GetEnvironmentUptimesResponse>(request, new { });
+
+        if (content == null) throw new AssertionException("Http response did not return a object");
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.That(content.EnvironmentUptimes.Keys.Count, Is.EqualTo(1));
+
+            Assert.That(content.EnvironmentUptimes[entity.Entity.Id], Is.Not.Null);
+            Assert.That(content.EnvironmentUptimes[entity.Entity.Id].ResourcesCount, Is.EqualTo(2));
+            Assert.That(content.EnvironmentUptimes[entity.Entity.Id].ScheduledStopTime, Is.EqualTo(scheduledStopTime1));
+        });
+    }
+
+    [Test]
+    public async Task Return_empty_list_when_no_environment_uptimes()
+    {
+        await SUT.Database.DeleteAllAsync<Resource>();
+        await SUT.Database.DeleteAllAsync<Environment>();
+        await SUT.Database.DeleteAllAsync<ResourceStopJob>();
+        
+        var request = new HttpRequestMessage(HttpMethod.Post, "/api/schedule/environment-uptimes/get");
+
+        var (response, content) = await SUT.SendHttpRequest<GetEnvironmentUptimesResponse>(request, new { });
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.That(content?.EnvironmentUptimes, Is.Not.Null);
+            CollectionAssert.IsEmpty(content?.EnvironmentUptimes.Keys);
+        });
+    }
+}
